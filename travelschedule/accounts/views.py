aff4a778978d826719a00f2ab34c14eaae828758
chihiro-mil <ex14.mil-ju0730@au.com>
2025-07-08 17:53:36 +0900

    accountsのforms作成、アカウント登録画面HTML作成

--- travelschedule/accounts/views.py
+++ travelschedule/accounts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth.models import User
 
 
 def register_view(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
 
 def login_view(request):
     return render(request, 'accounts/login.html')
