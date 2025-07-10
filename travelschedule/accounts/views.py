7266999cd1bfac001b2f2f599fe1786757bdc0c9
chihiro-mil <ex14.mil-ju0730@au.com>
2025-07-10 09:40:16 +0900

    7/10 9時30分までの状態を保存

--- travelschedule/accounts/views.py
+++ travelschedule/accounts/views.py
@@ -6,11 +6,16 @@ from django.contrib.auth.models import User
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
+        print("フォーム送信あり")
         if form.is_valid():
+            print("バリエーション成功")
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
             return redirect('/accounts/login/') #()の中は仮
+        else:
+            print("バリエーションNG!")
+            print(form.errors)
     else:
         form = RegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
