e8dcdae174b6bd4446dcf84b9d26d8ce049442d9
chihiro-mil <ex14.mil-ju0730@au.com>
2025-07-10 11:59:03 +0900

    標準UserかカスタムUserか選択前

--- travelschedule/accounts/models.py
+++ travelschedule/accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     name = models.CharField(max_length=20, unique=True)
     email = models.EmailField(max_length=255, unique=True)
+    USERNAME_FIELD = 'email'
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- travelschedule/accounts/views.py
+++ travelschedule/accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth.models import User
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
 
 def register_view(request):
     if request.method == 'POST':
@@ -22,6 +25,14 @@ def register_view(request):
 
 
 def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
     return render(request, 'accounts/login.html')
 
 def edit_profile_view(request):
