35690dc76fa0a5332a0d5aa8fc8a32cceef29ce9
chihiro-mil <ex14.mil-ju0730@au.com>
2025-08-11 18:32:58 +0900

    予定表編集モーダル　修正・確認済

--- travelschedule/app/templatetags/custom_filters.py
+++ travelschedule/app/templatetags/custom_filters.py
@@ -5,3 +5,9 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def break_every(value, length=10):
+    if not value:
+        return ''
+    return '<br>'.join([value[i:i+length] for i in range(0, len(value), length)])
