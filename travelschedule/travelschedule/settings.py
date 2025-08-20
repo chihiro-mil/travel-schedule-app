03576099d81797e805a4a2df6be2aef9cb2ed860
chihiro-mil <ex14.mil-ju0730@au.com>
2025-08-20 21:54:15 +0900

    ローカル上もDEBUG=Falseになるよう設定

--- travelschedule/travelschedule/settings.py
+++ travelschedule/travelschedule/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%yzh+(k4&3g-s3mofigq+2o+sf38!&u2!&$q%90mwu&l*oxfo7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'mil730.pythonanywhere.com'
+]
 
 
 # Application definition
